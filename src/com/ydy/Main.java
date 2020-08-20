ed0ac0d39985c9e24c0f8647518c0fa9493228fb
mikeyangdy <942091077@qq.com>
2020-08-20 21:03:18 +0800

    2222 push

--- src/com/ydy/Main.java
+++ src/com/ydy/Main.java
@@ -3,9 +3,14 @@
 public class Main{
     public static void main(String[] args) {
         System.out.println(1);
-
+        System.out.println(12);
+        System.out.println(13);
+        System.out.println(14);
+        System.out.println(15);
 
 
         System.out.println(233);
+
+        System.out.println(777777777777);
     }
 }
